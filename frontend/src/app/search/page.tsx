90369ccd148edf0a03a630498d6d651e02a87d95
Jayesh Gajbhar <jaygajbhar@gmail.com>
2024-10-31 15:40:47 -0400

    minor fix

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -39,7 +39,7 @@ export default function SearchPage() {
     }
 
     try {
-      const response = await fetch('/api/search', {
+      const response = await fetch('http://localhost:3000/search', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
